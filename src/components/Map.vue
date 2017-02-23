b5fd6f8cf15587f35b9641a40a1ccb111c4753f2
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2017-02-23 11:33:50 +0000

    Initial commit

---
+++ src/components/Map.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+  <div>
+    <h1>Map</h1>
+
+    <div id="myMap"></div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+
+      }
+    },
+    mounted: function() {
+      this.initMap();
+    },
+  methods: {
+    initMap() {
+      console.log('initMap');
+      map = new google.maps.Map(document.getElementById('myMap'), {
+                 center: {lat: 51.507351, lng: -0.127758},
+                 zoom: 12
+             });
+    }
+  }
+}
+</script>
+
+<style lang="css">
+#myMap {
+  height: 400px;
+  width: 400px;
+  border: 1px solid black;
+}
+</style>
