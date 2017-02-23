3b5f962cbb75cd5c9ce8dab2828c06801be6933b
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2017-02-23 16:26:20 +0000

    Styling

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,8 +1,9 @@
 <template lang="html">
-  <div>
-    <h1>Map</h1>
-
-    <div id="myMap"></div>
+  <div class='container'>
+    <h1>Map demo</h1>
+    <div id="myMap">
+      <p>Fetching location and setting up map ... </p>
+    </div>
   </div>
 </template>
 
@@ -10,45 +11,48 @@
 export default {
     data() {
       return {
-          lat: 51.507351,
-          lng: -0.127758,
+          lat: 0.0,
+          lng: 0.0,
           map: null
         }
     },
-    created(){
-      this.getCurrenttLocation();
-    },
 
     mounted: function() {
-      this.initMap();
+      this.getCurrentLocation()
+        .then(()=>{
+          this.initMap()
+        })
+        .catch((err)=>{
+          alert(err)
+        })
     },
 
     methods: {
-      getCurrenttLocation() {
-        if ('geolocation' in navigator) {
-          var gl = navigator.geolocation
-          gl.getCurrentPosition(function(position) {
-            console.log(position.coords);
-            this.lng = position.coords.longitude
-            this.lat = position.coords.latitude
+      getCurrentLocation() {
+        return new Promise((resolve, reject) =>{
+          if ('geolocation' in navigator) {
+            var gl = navigator.geolocation
+            gl.getCurrentPosition(function(position) {
+              this.lng = position.coords.longitude
+              this.lat = position.coords.latitude
 
-            let latLng = new google.maps.LatLng(this.lat, this.lng)
-            // this.map.setCenter(latLng);
+              let latLng = new google.maps.LatLng(this.lat, this.lng)
+            resolve(latLng);
 
-            this.map.panTo(latLng);
-
-          }.bind(this))
-        }
+            }.bind(this))
+          } else {
+            reject('Cannot use geolocation')
+          }
+        })
       },
 
       initMap() {
-        console.log('initMap');
         this.map = new google.maps.Map(document.getElementById('myMap'), {
                    center: {
                      lat: this.lat,
                      lng: this.lng
                    },
-                   zoom: 12
+                   zoom: 15
                });
       }
   }
@@ -56,9 +60,48 @@ export default {
 </script>
 
 <style lang="css">
+* {
+  margin: 0;
+  padding: 0;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  font-size: 15px;
+}
+
+html {
+  background-size: cover;
+  background-position: center;
+  max-width: 100vw;
+  height: 100vh;
+  background-color: #bdc3c7;
+}
+
+  h1 {
+    color: #e74c3c;
+    font-size: 4em;
+    font-weight: 400;
+    margin-top: 0.5em;
+    margin-bottom: 0.5em;
+  }
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
 #myMap {
-  height: 400px;
-  width: 400px;
-  border: 1px solid black;
+  height: 80vh;
+  width: 90vw;
+}
+
+#myMap p {
+  text-align: center;
+  font-size: 2em;
+  font-weight: 300;
+  line-height: 50vh;
+  color: #2c3e50;
 }
 </style>
